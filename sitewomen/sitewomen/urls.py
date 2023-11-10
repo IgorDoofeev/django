3f6fd9b29939999f5f8cde4e3e3d69ae74321d7b
igorDorofeev <gothar2010@yandex.ru>
2023-11-10 14:13:51 +0300

    add new urls

--- sitewomen/sitewomen/urls.py
+++ sitewomen/sitewomen/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from women import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
+    path('cats/', views.categories)
 ]
